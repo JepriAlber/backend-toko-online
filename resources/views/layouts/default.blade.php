73925288592b6cd0bba0f3b10f8387a9769d921a
JepriAlber <jeprialber@gmail.com>
2023-05-14 00:16:54 +0700

    membuat setup template untuk admin

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Toko Online - HTML5 Admin Template</title>
+    <meta name="description" content="ShaynaAdmin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- style --}}
+    @stack('style-before')
+    @include('includes.style')
+    @stack('style-after')
+
+</head>
+
+<body>
+    
+    {{-- sidebar --}}
+    @include('includes.sidebar')
+
+    <div id="right-panel" class="right-panel">
+        
+        {{-- navbar --}}
+        @include('includes.navbar')
+
+        <!-- Content -->
+        <div class="content">
+            
+            {{-- page (content) --}}
+            @yield('content')
+
+        </div>
+        <!-- /.content -->
+        <div class="clearfix"></div>
+    </div>
+
+    <!-- Scripts -->
+    @stack('script-before')
+    @include('includes.script')
+    @stack('script-after')
+
+</body>
+</html>
